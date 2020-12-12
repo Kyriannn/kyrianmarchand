59f9fa68523e3d7494ff33c28ff48a7ee4e2082c
Kyriannn <72595101+Kyriannn@users.noreply.github.com>
2020-12-12 19:38:51 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+function changeCSS (sheet) {
+	document.getElementById('pagestyle').setAttribute('href', sheet);
+}
